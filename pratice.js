3dac907d8a117ec3d5904703e72ca75de567254e
bhanu3009 <bhanuprakasht588@gmail.com>
2026-05-06 01:29:17 +0530

    Added home page, registration HTML, and basic JavaScript logic

---
+++ pratice.js
@@ -0,0 +1,22 @@
+// console.log("hi madam !")
+
+// let name="bhanu";
+
+// const namj="you cant change this";
+
+const bank_name="SecureBank";
+let Coustumer_name="bhanu prakash";
+let account_type="Savings";
+const coustumer_id='1001';
+let account_bal=1000;
+const limt=100000;
+let account_status =true;
+let credit_card=false;
+let last_due_date;
+let loan_officer;
+
+console.log("Welcome to :"+bank_name + "Mr"+name);
+console.log("your account is :"+account_type+" and you account Id is :"+coustumer_id);
+console.log("your account balance is :"+account_bal);
+console.log("your account status is :"+account_status);
+console.log("Do you have a credit card ? "+credit_card);
