5506b9c7704f80c1440b6cf34730d77b1278563a
bhanu3009 <bhanuprakasht588@gmail.com>
2026-05-09 02:28:13 +0530

    used the if else statment to check the eligibelity and make the transcations

--- pratice.js
+++ pratice.js
@@ -21,15 +21,33 @@
 // console.log("your account status is :"+account_status);
 // console.log("Do you have a credit card ? "+credit_card);
 
-const bank_name="Secure_Bank";
+// const name="bhanu_prakash";
+// let balance=10000;
+// let card=true;
+// let previous_loan=false;
+// let cbil=700;
+
+// if(balance!=500){
+//     console.log("your are not eligible for the loan because your balance is less than 500.")
+// }else if(balance>500 || balance<=10000 && cbil>=100 && previous_loan==false){
+//     console.log("You are eligible for the loan")
+// }else{
+//     console.log("Welcome to "+name);
+// }
+
+const bank_name="Secure_Bank",first_name="bhanu",last_name="prakash";
 const unique_id =1001;
-const first_name="bhanu";
-const last_name="prakash";
-let bank_balance=10000000;
-let kyc =true;
+let current_balance=100,transfer_limt=10000, request_transfer_amt=1100;
+let kyc =true,account_status=true,loan=null;
 let debit_card;
-let loan=null;
-console.log("Welcome to "+bank_name+"."+first_name+" "+last_name+". Your bank id is :"+unique_id+" and your amount which is in your bank account is :"+bank_balance+". You have loan ?"+loan+".");
-
-
-
+console.log("Welcome to "+bank_name+"."+first_name+" "+last_name+". Your bank id is :"+unique_id+" and your amount which is in your bank account is :"+current_balance+". You have loan ?"+loan+".");
+if(account_status!=true){
+    console.log("Your tarnsetion is denied due to inactive account. ");
+}else if(account_status==true && request_transfer_amt==transfer_limt){
+    console.log ("your limt is insufficent to transfer the balance.");
+}else if(request_transfer_amt>=current_balance){
+    console.log("You dont have sufficent funds to transfer money.")
+}else{
+    let temp=current_balance-request_transfer_amt;
+    console.log("successfully Transferd and your current balance is :"+temp+".");
+}
