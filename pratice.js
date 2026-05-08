9e49bb9e649bf035a7ce73e4c563ac9f39ff87db
bhanu3009 <bhanuprakasht588@gmail.com>
2026-05-09 00:18:47 +0530

    Day 1 for Secure_Banking project.

--- pratice.js
+++ pratice.js
@@ -4,19 +4,32 @@
 
 // const namj="you cant change this";
 
-const bank_name="SecureBank";
-let Coustumer_name="bhanu prakash";
-let account_type="Savings";
-const coustumer_id='1001';
-let account_bal=1000;
-const limt=100000;
-let account_status =true;
-let credit_card=false;
-let last_due_date;
-let loan_officer;
+// const bank_name="SecureBank";
+// let Coustumer_name="bhanu prakash";
+// let account_type="Savings";
+// const coustumer_id='1001';
+// let account_bal=1000;
+// const limt=100000;
+// let account_status =true;
+// let credit_card=false;
+// let last_due_date;
+// let loan_officer;
 
-console.log("Welcome to :"+bank_name + "Mr"+name);
-console.log("your account is :"+account_type+" and you account Id is :"+coustumer_id);
-console.log("your account balance is :"+account_bal);
-console.log("your account status is :"+account_status);
-console.log("Do you have a credit card ? "+credit_card);
+// console.log("Welcome to :"+bank_name + "Mr"+name);
+// console.log("your account is :"+account_type+" and you account Id is :"+coustumer_id);
+// console.log("your account balance is :"+account_bal);
+// console.log("your account status is :"+account_status);
+// console.log("Do you have a credit card ? "+credit_card);
+
+const bank_name="Secure_Bank";
+const unique_id =1001;
+const first_name="bhanu";
+const last_name="prakash";
+let bank_balance=10000000;
+let kyc =true;
+let debit_card;
+let loan=null;
+console.log("Welcome to "+bank_name+"."+first_name+" "+last_name+". Your bank id is :"+unique_id+" and your amount which is in your bank account is :"+bank_balance+". You have loan ?"+loan+".");
+
+
+
