4e47275724a4a5bba6c2f747c288d583e604b97a
bhanu3009 <bhanuprakasht588@gmail.com>
2026-05-12 22:27:05 +0530

    by combing all the basic varble and loops i had succesfully done the task by showing this output for the task

--- pratice.js
+++ pratice.js
@@ -35,19 +35,78 @@
 //     console.log("Welcome to "+name);
 // }
 
-const bank_name="Secure_Bank",first_name="bhanu",last_name="prakash";
-const unique_id =1001;
-let current_balance=100,transfer_limt=10000, request_transfer_amt=1100;
-let kyc =true,account_status=true,loan=null;
-let debit_card;
-console.log("Welcome to "+bank_name+"."+first_name+" "+last_name+". Your bank id is :"+unique_id+" and your amount which is in your bank account is :"+current_balance+". You have loan ?"+loan+".");
-if(account_status!=true){
-    console.log("Your tarnsetion is denied due to inactive account. ");
-}else if(account_status==true && request_transfer_amt==transfer_limt){
-    console.log ("your limt is insufficent to transfer the balance.");
-}else if(request_transfer_amt>=current_balance){
-    console.log("You dont have sufficent funds to transfer money.")
-}else{
-    let temp=current_balance-request_transfer_amt;
-    console.log("successfully Transferd and your current balance is :"+temp+".");
+// const bank_name="Secure_Bank",first_name="bhanu",last_name="prakash";
+// const unique_id =1001;
+// let current_balance=100,transfer_limt=10000, request_transfer_amt=1100;
+// let kyc =true,account_status=true,loan=null;
+// let debit_card;
+// console.log("Welcome to "+bank_name+"."+first_name+" "+last_name+". Your bank id is :"+unique_id+" and your amount which is in your bank account is :"+current_balance+". You have loan ?"+loan+".");
+// if(account_status!=true){
+//     console.log("Your tarnsetion is denied due to inactive account. ");
+// }else if(account_status==true && request_transfer_amt==transfer_limt){
+//     console.log ("your limt is insufficent to transfer the balance.");
+// }else if(request_transfer_amt>=current_balance){
+//     console.log("You dont have sufficent funds to transfer money.")
+// }else{
+//     let temp=current_balance-request_transfer_amt;
+//     console.log("successfully Transferd and your current balance is :"+temp+".");
+// }
+
+
+// let arr=["bhanu","thanu","janu","vanu"];
+// console.log("You array are :"+arr);
+// console.log(arr[1]);
+// arr.push("hi");
+// console.log("Your array after pushing the hi :"+arr)
+// arr.pop();
+// console.log("YOu array after pop :"+arr);
+// arr.unshift("special")
+// console.log("YOu array after adding the special one :"+arr);
+// arr.shift();
+// console.log("YOu array after removing the special one :"+arr);
+// arr.splice(1,1);
+// console.log("YOu final arrays is :"+arr);
+// arr.reverse;
+// console.log("your reverse array is :"+arr);
+
+
+// let arr=[10,20,30,40,50,600,70,80,90];
+// let result=0,large=0;
+// for(let i=0;i<arr.length;i++){
+//     result=result+arr[i];
+//     if(large<=arr[i])
+//         large=arr[i];
+// }
+// console.log(result);
+// console.log(large)
+
+
+// The Execution:
+
+// Set the Stage: Declare a sealed variable for a dailyLimit (e.g., 20000). Declare a flexible variable for the currentBalance (e.g., 50000).
+
+// The Data: Create an array containing four pending withdrawal requests (e.g., 5000, 25000, 10000, 2000).
+
+// The Processor: Write a for loop that is designed to examine every single item in your array, one by one.
+
+// The Logic Gate (Inside the Loop): Inside your loop, place an if/else condition. For every transaction amount the loop looks at, it must check:
+
+// Is the requested amount less than or equal to the currentBalance AND less than or equal to the dailyLimit?
+
+// If true: Subtract the amount from the currentBalance, deduct the amount from the remaining dailyLimit, and print a message saying "Transaction of [Amount] Approved. New Balance: [Balance]".
+
+// If false: Do not change the balance or the limit. Print a message saying "Transaction of [Amount] Declined due to insufficient funds or limit breach."
+
+const dailyLimit=20000;
+let currentBalance=10000;
+let remaining_limt=dailyLimit;
+let pending_withdrawa=[5000, 25000, 10000, 2000];
+for(let i=0;i<pending_withdrawa.length;i++){
+    if(pending_withdrawa[i]<=currentBalance && pending_withdrawa[i]<=dailyLimit){
+    currentBalance=currentBalance-pending_withdrawa[i];
+    remaining_limt=pending_withdrawa[i]-remaining_limt;
+    console.log("Transaction of :"+pending_withdrawa[i]+" Approved. New Balance :"+pending_withdrawa[i]);
+    }else{
+        console.log("transtion declined");
+    }
 }
